28618e43d0c01f9fc87ced921154184f030ef838
mcpe500 <ivan04.android@gmail.com>
2024-07-09 15:46:27 +0700

    created the login and register and forgot password page

--- my-kanban-frontend/src/pages/auth/login.tsx
+++ my-kanban-frontend/src/pages/auth/login.tsx
@@ -1,12 +1,54 @@
 // pages/auth/login.tsx
-import React from 'react';
+import {
+    Anchor,
+    Button,
+    Checkbox,
+    Container,
+    Group,
+    Paper,
+    PasswordInput,
+    Text,
+    TextInput,
+    Title,
+} from "@mantine/core";
+import React from "react";
+import classes from "./AuthenticationPage.module.css";
+import Link from "next/link";
 
 const LoginPage: React.FC = () => {
     return (
-        <div>
-            <h1>Login Page</h1>
-            {/* Your login form components */}
-        </div>
+        <Container size={420} my={40}>
+            <Title ta="center" className={classes.title}>
+                Welcome back!
+            </Title>
+            <Text c="dimmed" size="sm" ta="center" mt={5}>
+                Do not have an account yet?{" "}
+                <Anchor size="sm" component="button">
+                    <Link href={"/auth/register"}>Create account</Link>
+
+                </Anchor>
+            </Text>
+
+            <Paper withBorder shadow="md" p={30} mt={30} radius="md">
+                <TextInput label="Email" placeholder="you@mantine.dev" required />
+                <PasswordInput
+                    label="Password"
+                    placeholder="Your password"
+                    required
+                    mt="md"
+                />
+                <Group justify="space-between" mt="lg">
+                    <Checkbox label="Remember me" />
+                    <Anchor component="button" size="sm">
+                        <Link href={"/auth/forgot_password"}>Forgot password?</Link>
+                    </Anchor>
+                </Group>
+
+                <Button fullWidth mt="xl">
+                    Sign in
+                </Button>
+            </Paper>
+        </Container>
     );
 };
 
--- my-kanban-frontend/src/pages/auth/register.tsx
+++ my-kanban-frontend/src/pages/auth/register.tsx
@@ -1,12 +1,32 @@
 // pages/auth/register.tsx
 import React from 'react';
+import { Container, Paper, TextInput, PasswordInput, Button, Checkbox, Text, Title, Anchor } from '@mantine/core';
+import Link from 'next/link';
+import classes from './AuthenticationPage.module.css';
 
 const RegisterPage: React.FC = () => {
     return (
-        <div>
-            <h1>Register Page</h1>
-            {/* Your registration form components */}
-        </div>
+        <Container size={420} my={40}>
+            <Title ta="center" className={classes.title}>
+                Create an Account
+            </Title>
+            <Text c="dimmed" size="sm" ta="center" mt={5}>
+                Already have an account?{' '}
+                <Anchor size="sm" component="button">
+                    <Link href="/auth/login">Sign in</Link>
+                </Anchor>
+            </Text>
+
+            <Paper withBorder shadow="md" p={30} mt={30} radius="md">
+                <TextInput label="Email" placeholder="Enter your email address" required />
+                <PasswordInput label="Password" placeholder="Choose a strong password" required mt="md" />
+                {/* <Checkbox label="Remember me" /> */}
+
+                <Button fullWidth mt="xl" variant="gradient">
+                    Create Account
+                </Button>
+            </Paper>
+        </Container>
     );
 };
 
